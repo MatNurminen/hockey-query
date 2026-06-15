98894d7e001379d871ef3ac21009f3d854fc55c6
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-06-16 00:32:53 +0300

    refactor searchPlayer

--- src/components/common/SearchPlayer/index.tsx
+++ src/components/common/SearchPlayer/index.tsx
@@ -1,57 +1,58 @@
-import { useState } from 'react';
-import { useDebounce } from 'use-debounce';
-import Autocomplete from '@mui/material/Autocomplete';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import TableFlag from '../../common/Images/tableFlag';
-import { getPlayers } from '../../../api/players/queries';
-import { TPlayerDto } from '../../../api/players/types';
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import TableFlag from "../../common/Images/tableFlag";
+import { getPlayers } from "../../../api/players/queries";
+import { TPlayerDto } from "../../../api/players/types";
 
 type Props = {
   onPlayerSelect: (playerId: number) => void;
 };
 
 const SearchPlayer = ({ onPlayerSelect }: Props) => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [value, setValue] = useState<TPlayerDto | null>(null);
   const [debouncedInput] = useDebounce(inputValue, 400);
   const enabled = debouncedInput.length > 2;
 
   const { data: players = [], isFetching } = getPlayers(
     debouncedInput,
-    enabled
+    enabled,
   );
 
   return (
     <Autocomplete
       freeSolo
-      id='player-search'
+      id="player-search"
       options={players}
       loading={isFetching}
       inputValue={inputValue}
       onInputChange={(_, value) => setInputValue(value)}
       value={value}
       getOptionLabel={(option) =>
-        typeof option === 'string'
+        typeof option === "string"
           ? option
-          : `${option.first_name ?? ''} ${option.last_name ?? ''}`
+          : `${option.first_name ?? ""} ${option.last_name ?? ""}`
       }
       onChange={(_, newValue) => {
-        if (newValue && typeof newValue !== 'string') {
+        if (newValue && typeof newValue !== "string") {
           onPlayerSelect(newValue.id);
-          setInputValue('');
+          setInputValue("");
           setValue(null);
         }
       }}
       renderOption={(props, option) => {
+        const { key, ...rest } = props;
         return (
           <Box
-            component='li'
-            {...props}
-            key={option.id}
-            sx={{ display: 'flex', alignItems: 'center' }}
+            component="li"
+            key={key}
+            {...rest}
+            sx={{ display: "flex", alignItems: "center" }}
           >
-            <Box display='flex' sx={{ mr: 1 }}>
+            <Box display="flex" sx={{ mr: 1 }}>
               <TableFlag
                 src={option.nation.flag}
                 alt={`${option.first_name} ${option.last_name} flag`}
@@ -62,10 +63,10 @@ const SearchPlayer = ({ onPlayerSelect }: Props) => {
           </Box>
         );
       }}
-      sx={{ width: '100%', backgroundColor: '#fff' }}
-      open={debouncedInput.length > 2 && !isFetching && players.length > 0}
+      sx={{ width: "100%", backgroundColor: "background.paper" }}
+      open={enabled && !isFetching && players.length > 0}
       renderInput={(params) => (
-        <TextField {...params} label='Search players' size='small' />
+        <TextField {...params} label="Search players" size="small" />
       )}
     />
   );
