3d2aebc32cd01cb62aec20b02b1a54d90790f541
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-06-15 22:41:43 +0300

    refactor deleteDialog

--- src/components/common/Dialogs/deleteDialog.tsx
+++ src/components/common/Dialogs/deleteDialog.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
-import AppButton from '../Buttons/appButton';
-import GrayButton from '../Buttons/grayButton';
-import Stack from '@mui/material/Stack';
-import DialogContent from '@mui/material/DialogContent';
-import Typography from '@mui/material/Typography';
-import WarningIcon from '@mui/icons-material/Warning';
-import CircularProgress from '@mui/material/CircularProgress';
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import AppButton from "../Buttons/appButton";
+import GrayButton from "../Buttons/grayButton";
+import Stack from "@mui/material/Stack";
+import DialogContent from "@mui/material/DialogContent";
+import Typography from "@mui/material/Typography";
+import WarningIcon from "@mui/icons-material/Warning";
 
 const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>
+  props: TransitionProps & { children: React.ReactElement },
+  ref: React.Ref<unknown>,
 ) {
-  return <Slide direction='up' ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 type DeleteDialogProps = {
@@ -34,50 +33,52 @@ export default function DeleteDialog({
   onConfirm,
   loading = false,
 }: DeleteDialogProps) {
-  return (      <Dialog
+  const handleDelete = () => {
+    onConfirm();
+    onClose();
+  };
+
+  return (
+    <Dialog
       open={open}
       slots={{ transition: Transition }}
       keepMounted
       onClose={onClose}
       disableRestoreFocus
-      role='alertdialog'
-      aria-labelledby='delete-dialog-title'
-      aria-describedby='delete-dialog-description'
-      aria-modal='true'
-      maxWidth='sm'
+      role="alertdialog"
+      aria-labelledby="delete-dialog-title"
+      aria-describedby="delete-dialog-description"
+      aria-modal="true"
+      maxWidth="sm"
       fullWidth
     >
-      <DialogTitle id='delete-dialog-title'>
-        <Stack direction='row' spacing={1} alignItems='center'>
-          <WarningIcon color='warning' />
+      <DialogTitle id="delete-dialog-title">
+        <Stack direction="row" spacing={1} alignItems="center">
+          <WarningIcon color="warning" />
           <Typography>Do you want to delete '{name}'?</Typography>
         </Stack>
       </DialogTitle>
       <DialogContent>
-        <Typography id='delete-dialog-description'>
+        <Typography id="delete-dialog-description">
           This action can't be undone. '{name}' will be permanently deleted!
         </Typography>
       </DialogContent>
       <DialogActions>
-        <Stack direction='row' spacing={2} sx={{ mb: 2 }}>
+        <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
           <GrayButton
-            text='Cancel'
-            size='small'
+            text="Cancel"
+            size="small"
             onClick={onClose}
             disabled={loading}
             autoFocus
           />
           <AppButton
-            text={loading ? 'Deleting...' : 'Delete'}
-            size='small'
-            color='error'
-            onClick={() => {
-              onConfirm();
-              onClose();
-            }}
+            text={loading ? "Deleting..." : "Delete"}
+            size="small"
+            color="error"
+            onClick={handleDelete}
             disabled={loading}
           />
-          {loading && <CircularProgress size={20} sx={{ ml: 1 }} />}
         </Stack>
       </DialogActions>
     </Dialog>
