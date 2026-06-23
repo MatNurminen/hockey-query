a366872c1a743e19570ba98b437e7db0854dd5fb
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-06-23 17:33:24 +0300

    refactor notFound component

--- src/components/notFound/index.tsx
+++ src/components/notFound/index.tsx
@@ -1,33 +1,33 @@
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid2';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import CardMedia from '@mui/material/CardMedia';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
+import { memo } from "react";
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <Box sx={{ padding: 4, backgroundColor: '#063950' }}>
+    <Box sx={{ padding: 4, backgroundColor: "#063950" }}>
       <Container>
-        <Grid container alignItems='center'>
-          <Grid size={1}></Grid>
+        <Grid container justifyContent="center" alignItems="center">
           <Grid size={4}>
-            <Typography sx={{ color: '#fff' }} variant='h1'>
-              <Box fontWeight='fontWeightBold'>404</Box>
+            <Typography sx={{ color: "#fff" }} variant="h1" fontWeight="bold">
+              404
             </Typography>
-            <Typography sx={{ color: '#fff' }} variant='h4'>
+            <Typography sx={{ color: "#fff" }} variant="h4">
               Not found
             </Typography>
-            <Typography sx={{ color: '#fff' }}>
+            <Typography sx={{ color: "#fff" }}>
               The link is broken or the page has been moved.
             </Typography>
           </Grid>
           <Grid size={7}>
-            <CardMedia sx={{ height: 300 }} image='/img/puck_spinning.gif' />
+            <CardMedia sx={{ height: 300 }} image="/img/puck_spinning.gif" />
           </Grid>
         </Grid>
       </Container>
     </Box>
   );
-}
+};
 
-export default NotFound;
+export default memo(NotFound);
