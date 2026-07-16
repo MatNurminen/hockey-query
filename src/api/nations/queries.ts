4e4b357e45562c474b253d4cd5fb4136b8bd85f3
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-07-17 01:26:47 +0300

    refact api nations

--- src/api/nations/queries.ts
+++ src/api/nations/queries.ts
@@ -1,10 +1,10 @@
-import { createQuery } from '../factories/queryFactory';
-import { TNationDto } from './types';
+import { createQuery } from "../factories/queryFactory";
+import { TNationDto } from "./types";
 
 export const getNations = () => {
-  return createQuery<TNationDto[]>(['nations'], '/api/nations');
+  return createQuery<TNationDto[]>(["nations"], "/api/nations");
 };
 
 export const getNation = (id: number) => {
-  return createQuery<TNationDto>(['nation', id], `/api/nations/${id}`);
+  return createQuery<TNationDto>(["nation", id], `/api/nations/${id}`);
 };
