44e1cb975a1ce9658a0f764e219a3fe7587b9630
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-09-10 13:28:52 +0300

    add lazy import into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,49 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import "./App.css";
+
 import { ThemeProvider } from "@mui/material/styles";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-import theme from "./components/ui/Theme";
-import Main from "./components/main";
-import NotFound from "./components/notFound";
-import Navbar from "./components/layout/Navbar";
-import AdminNavbar from "./components/layout/AdminNavbar";
-import Leagues from "./components/leagues/leagues";
-import League from "./components/leagues/league";
-import PlayersStats from "./components/leagues/stats";
-import Nations from "./components/nations/nations";
-import Nation from "./components/nations/nation";
-import NationStats from "./components/nations/stats";
-import Teams from "./components/teams/teams";
-import Loading from "./components/layout/React-query/loading";
+import { Routes, Route } from "react-router";
 
 import queryClient from "./api/queryClient";
-import Team from "./components/teams/team";
-import FreeAgents from "./components/players/free-agents";
-import Drafts from "./components/players/drafts";
-import DraftDetails from "./components/players/drafts/draftDetails";
-import Player from "./components/players/player";
+
+import theme from "./components/ui/Theme";
+
+import Navbar from "./components/layout/Navbar";
+import AdminNavbar from "./components/layout/AdminNavbar";
 import SearchBar from "./components/layout/SearchBar";
-import Tournaments from "./components/tournaments";
-import Tournament from "./components/admin/tournaments/updateTournament";
-import Rosters from "./components/rosters";
-import AdmRosters from "./components/admin/rosters";
+import Loading from "./components/layout/React-query/loading";
 import Footer from "./components/layout/Footer";
+
+const Main = lazy(() => import("./components/main"));
+const NotFound = lazy(() => import("./components/notFound"));
+
+const Leagues = lazy(() => import("./components/leagues/leagues"));
+const League = lazy(() => import("./components/leagues/league"));
+const PlayersStats = lazy(() => import("./components/leagues/stats"));
+
+const Nations = lazy(() => import("./components/nations/nations"));
+const Nation = lazy(() => import("./components/nations/nation"));
+const NationStats = lazy(() => import("./components/nations/stats"));
+
+const Teams = lazy(() => import("./components/teams/teams"));
+const Team = lazy(() => import("./components/teams/team"));
+
+const Player = lazy(() => import("./components/players/player"));
+const FreeAgents = lazy(() => import("./components/players/free-agents"));
+const Drafts = lazy(() => import("./components/players/drafts"));
+const DraftDetails = lazy(
+  () => import("./components/players/drafts/draftDetails"),
+);
+
+const Tournaments = lazy(() => import("./components/tournaments"));
+const Tournament = lazy(
+  () => import("./components/admin/tournaments/updateTournament"),
+);
+
+const Rosters = lazy(() => import("./components/rosters"));
+const AdmRosters = lazy(() => import("./components/admin/rosters"));
 
 function App() {
   return (
@@ -40,33 +53,42 @@ function App() {
         <AdminNavbar />
         <SearchBar />
         <Loading />
+
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Main />} />
+
             <Route path="/leagues" element={<Leagues />} />
             <Route path="/leagues/:id" element={<League />} />
             <Route path="/league-stats" element={<PlayersStats />} />
+
             <Route path="/nations" element={<Nations />} />
             <Route path="/nations/:id" element={<Nation />} />
-            <Route path="/teams" element={<Teams />} />
-            <Route path="/teams/:id" element={<Team />} />
-            <Route path="/players/:id" element={<Player />} />
-            <Route path="/free-agents" element={<FreeAgents />} />
-            <Route path="/drafts" element={<Drafts />} />
-            <Route path="/drafts/dets" element={<DraftDetails />} />
-            <Route path="/tournaments" element={<Tournaments />} />
-            <Route path="/tournaments/:id" element={<Tournament />} />
-            <Route path="/rosters" element={<Rosters />} />
             <Route path="/nation" element={<NationStats />} />
 
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/teams/:id" element={<Team />} />
+
+            <Route path="/players/:id" element={<Player />} />
+            <Route path="/free-agents" element={<FreeAgents />} />
+
+            <Route path="/drafts" element={<Drafts />} />
+            <Route path="/drafts/dets" element={<DraftDetails />} />
+
+            <Route path="/tournaments" element={<Tournaments />} />
+            <Route path="/tournaments/:id" element={<Tournament />} />
+
+            <Route path="/rosters" element={<Rosters />} />
             <Route path="/adm-rosters" element={<AdmRosters />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
+
         <Footer />
       </ThemeProvider>
-      <ReactQueryDevtools />
+
+      {import.meta.env.DEV && <ReactQueryDevtools />}
     </QueryClientProvider>
   );
 }
