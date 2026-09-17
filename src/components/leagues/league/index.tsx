9a14ff746d373e18b55c83252f09a4109990c9b5
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-09-18 01:40:59 +0300

    correct components src/components/leagues with new postseason

--- src/components/leagues/league/index.tsx
+++ src/components/leagues/league/index.tsx
@@ -68,22 +68,22 @@ const League = () => {
       <Paper sx={{ mt: 2 }}>
         <CompareTeams leagueId={leagueId} seasonId={seasonId} title={title} />
       </Paper>
-       <Paper sx={{ mt: 2 }}>
+      <Paper sx={{ mt: 2 }}>
         <PlayersFacts leagueId={leagueId} seasonId={seasonId} />
       </Paper>
       <Paper sx={{ mt: 4, backgroundColor: "transparent", boxShadow: "none" }}>
         <PlayersStatsTotal leagueId={leagueId} seasonId={seasonId} />
       </Paper>
-      
+
       <Paper sx={{ mt: 2, backgroundColor: "transparent", boxShadow: "none" }}>
         <PlayersStatsPerSeason leagueId={leagueId} seasonId={seasonId} />
       </Paper>
       <Paper sx={{ mt: 2 }}>
         <Champions title={title} leagueId={leagueId} />
       </Paper>
-      {/* <Paper sx={{ mt: 2 }}>
+      <Paper sx={{ mt: 2 }}>
         <Awards title={title} />
-      </Paper> */}
+      </Paper>
     </Container>
   );
 };
