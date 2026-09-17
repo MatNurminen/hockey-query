89e19af858c7cb1d5670584c1713dfa8efb27538
Matvey Nurminen <mat.nurminen@hotmail.com>
2026-09-17 03:13:39 +0300

    awards and postseason types into api

---
+++ src/api/awards/types.ts
@@ -0,0 +1,10 @@
+export type TAwardDto = {
+  id: number;
+  league_id: number;
+  name: string;
+};
+
+export type TCreateAwardDto = {
+  league_id: number;
+  name: string;
+};
---
+++ src/api/postseason/types.ts
@@ -0,0 +1,8 @@
+export type TPostseasonDto = {
+  id: number;
+  name: string;
+};
+
+export type TCreatePostseasonDto = {
+  name: string;
+};
